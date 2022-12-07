e80294e60355bcb741872ced91ebc77d5e16c510
Autorua <88741033+Autorua@users.noreply.github.com>
2022-12-07 22:10:47 +0200

    inbot
    inbot

---
+++ config.py
@@ -0,0 +1,9 @@
+username = "thecatsw0rld"
+password = "081257ss8s"
+
+#username = "january.fyi1"
+#password = "150279ss081257QQ"
+
+
+#username = "december.fyi"
+#password = "150279ss8s77QQ88"
---
+++ userfol_by_htag.py
@@ -0,0 +1,34 @@
+from instagrapi import Client
+import config
+import time
+import random
+
+cl = Client()
+cl.login(config.username, config.password)
+
+hashtag = "richmond"
+amount = 15
+posts = cl.hashtag_medias_recent(hashtag,amount)
+#posts = cl.hashtag_medias_top(hashtag, amount)
+
+while True:
+
+    try:
+
+        for i in range(amount):
+            print("Post" + str(i+1))
+            cl.media_like(posts[i].id)
+            print("Liked post: " + posts[i].id)
+            cl.user_follow(posts[i].user.pk)
+            print("Followed: " + posts[i].user.username)
+            time.sleep(random.randint(58, 123))
+
+    except Exception as ex:
+        print(ex)
+        continue
+
+
+
+
+
+
