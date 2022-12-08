9d29b73d472ae0ed29e0ef7f702c864a8a9b9719
Autorua <88741033+Autorua@users.noreply.github.com>
2022-12-08 07:32:44 +0200

    Update userfol_by_htag.py

--- userfol_by_htag.py
+++ userfol_by_htag.py
@@ -7,7 +7,7 @@ cl = Client()
 cl.login(config.username, config.password)
 
 hashtag = "birmingham"
-amount = 1000
+amount = 25
 posts = cl.hashtag_medias_recent(hashtag,amount)
 #posts = cl.hashtag_medias_top(hashtag, amount)
 
